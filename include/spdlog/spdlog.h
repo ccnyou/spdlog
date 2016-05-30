7fc832a7484e04682704e68cb1e5f1491ada3572
ewanshin <ewanshin@gmail.com>
2016-05-30 20:48:11 +0900

    * merge hourly log

--- include/spdlog/spdlog.h
+++ include/spdlog/spdlog.h
@@ -87,6 +87,12 @@ std::shared_ptr<logger> daily_logger_mt(const std::string& logger_name, const fi
 std::shared_ptr<logger> daily_logger_st(const std::string& logger_name, const filename_t& filename, int hour=0, int minute=0);
 
 //
+// Create file logger which creates new file on the given hour:
+//
+std::shared_ptr<logger> hourly_logger_mt(const std::string& logger_name, const filename_t& filename, bool force_flush = false);
+std::shared_ptr<logger> hourly_logger_st(const std::string& logger_name, const filename_t& filename, bool force_flush = false);
+
+//
 // Create and register stdout/stderr loggers
 //
 std::shared_ptr<logger> stdout_logger_mt(const std::string& logger_name);
