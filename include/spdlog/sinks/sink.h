85b9e009d7b34f3dcd29b0c4e8d8e5463d6acda2
chencongning <chencongning@bytedance.com>
2021-01-29 16:25:33 +0800

    * merge from gabime at 20210129

--- include/spdlog/sinks/sink.h
+++ include/spdlog/sinks/sink.h
@@ -1,54 +1,35 @@
-//
-// Copyright(c) 2015 Gabi Melman.
+// Copyright(c) 2015-present, Gabi Melman & spdlog contributors.
 // Distributed under the MIT License (http://opensource.org/licenses/MIT)
-//
 
 #pragma once
 
-#include "../details/log_msg.h"
+#include <spdlog/details/log_msg.h>
+#include <spdlog/formatter.h>
 
 namespace spdlog {
+
 namespace sinks {
-class sink
+class SPDLOG_API sink
 {
 public:
     virtual ~sink() = default;
-
     virtual void log(const details::log_msg &msg) = 0;
     virtual void flush() = 0;
+    virtual void set_pattern(const std::string &pattern) = 0;
+    virtual void set_formatter(std::unique_ptr<spdlog::formatter> sink_formatter) = 0;
 
-    bool should_log(level::level_enum msg_level) const;
     void set_level(level::level_enum log_level);
     level::level_enum level() const;
+    bool should_log(level::level_enum msg_level) const;
 
-    // 目前只有 hourly_file_sink 实现
-    virtual const filename_t calc_log_file_name(const std::tm& time) const;
-    
-private:
-    level_t _level{level::trace};
+protected:
+    // sink log level - default is all
+    level_t level_{level::trace};
 };
 
-inline bool sink::should_log(level::level_enum msg_level) const
-{
-    return msg_level >= _level.load(std::memory_order_relaxed);
-}
+} // namespace sinks
+} // namespace spdlog
 
-inline void sink::set_level(level::level_enum log_level)
-{
-    _level.store(log_level);
-}
-
-inline level::level_enum sink::level() const
-{
-    return static_cast<spdlog::level::level_enum>(_level.load(std::memory_order_relaxed));
-}
-
-// 默认实现，这里应该调到子类才对
-inline const filename_t sink::calc_log_file_name(const std::tm& time) const {
-    static filename_t result;
-    return result;
-}
-    
-}
-}
-
+#ifdef SPDLOG_HEADER_ONLY
+#include "sink-inl.h"
+#endif
